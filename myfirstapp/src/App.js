ca66ce57bc9dff65ffbf00c57cba0cab7d972989
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-14 12:38:25 +0300

    I have reset everything in the reactJS file

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+
         <h1 className='title-h1'>This is my first program</h1>
         <a
           className="App-link"
