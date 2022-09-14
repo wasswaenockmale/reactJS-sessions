9b78918483cb03e113ce99a6a0eaca4ab6d02cca
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-14 12:31:12 +0300

    I have removed the comments that I have made

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
+        
         <h1 className='title-h1'>This is my first program</h1>
         <a
           className="App-link"
