dbe65c006436dcb4d17a69cc3c018e9741358306
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-15 15:32:01 +0300

    Added a new react file in the freecodecamp folder

---
+++ freecodecamp/complexElement.jsx
@@ -0,0 +1,9 @@
+const JSX = <div>
+<h1>First complex element</h1>
+<p>It has elements listed below</p>
+<ul>
+<li>h1 element tag</li>
+<li>p element tag</li>
+<li>unordered list tag</li>
+</ul>
+</div>
