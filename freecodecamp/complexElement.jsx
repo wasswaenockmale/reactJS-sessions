36673084b00a5f349855e9e5f124acccc61bd938
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-20 11:38:39 +0300

    Learnt how to create a stateless component in react

--- freecodecamp/complexElement.jsx
+++ freecodecamp/complexElement.jsx
@@ -26,3 +26,23 @@ const comp = <div>
     <p>Be sure to close all tags!</p>
     <hr/>
 </div>
+
+// Everything in react is a component 
+// There are two ways to create a componet in jsx.
+// One is to use the javascript function.
+// Doing this, lets you create a stateless component.
+// Learning how to create a stateless component
+// What are stateless components in react 
+// To create a component with a function, 
+// you simply write a JavaScript function that returns either JSX or null. 
+// One important thing to note is that React requires your function name 
+// to begin with a capital letter. Here's an example of a stateless functional 
+// component that assigns an HTML class in JSX:
+
+const DemoComponent = function(){
+    return (
+        <div className="customClass">
+            <h3 className="title">Stateless component</h3>
+        </div>
+    );
+}
