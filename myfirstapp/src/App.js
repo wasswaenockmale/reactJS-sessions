c503dc5390b4f05ca435285abb6b9ce108a69715
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-20 11:04:27 +0300

    I have added an id to the returned div in App.js file

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id='main-element'>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className='title-h1'>This is my first program</h1>
