7c70ba00fa4c29b3ec42005ac78b292d5e3b6f0b
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-20 23:03:45 +0300

    I have created two statelss components

--- freecodecamp/passprops.jsx
+++ freecodecamp/passprops.jsx
@@ -1,1 +1,15 @@
 // Passing properties to a stateless functional Component. 
+const Welcome = (props)=>{
+    <div>
+        <p>You are most welcome {props.user}</p>
+    </div>
+}
+
+// This is another stateless component.
+const Now = (props)=>{
+    <div>
+        <p>
+            Today is {props.date}
+        </p>
+    </div>
+}
