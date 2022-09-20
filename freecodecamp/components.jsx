60fbd4d2d6af42812ee02abddc05b95436c248fc
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-20 12:45:24 +0300

    I have added a new file that uses the two ways of compositions components in react. and also uses the two ways of creating components in react

---
+++ freecodecamp/components.jsx
@@ -0,0 +1,38 @@
+// This is a functional Component.
+const Languages = ()=>{
+    return(
+        <div>
+            <ul>
+                <li>Java</li>
+                <li>Python</li>
+                <li>Javascript</li>
+                <li>HTML</li>
+            </ul>
+        </div>
+    )
+}
+
+// Another functional component.
+const Programming = ()=>{
+    // {/*This component is going to use the composition theory in react. */}
+    return(
+        <div>
+            {/* This is where the child is going. */}
+            <Languages/>
+        </div>
+    )
+}
+
+// this is a class component 
+class ParentComp extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        <div>
+            {/* Still writing the child here  */}
+            <Programming/>
+        </div>
+    }
+}
