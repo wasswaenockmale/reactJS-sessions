6e69acea53ebb8f54b75065e8e0151697dc9ce2e
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-20 11:11:42 +0300

    using the self-closing tags in jsx

--- freecodecamp/complexElement.jsx
+++ freecodecamp/complexElement.jsx
@@ -13,3 +13,16 @@ const JXS = <div><p>This is gonna be rendered with ReactDOM.render(componentToRe
 
 // Forexample, if there is a div node with an id='example-challenge'
 ReactDOM.render(JXS, document.getElementById("example-challenge"))
+
+// attribute naming in jsx becomes camelCase
+// self-closing tags in jsx 
+// In JSX, the rules are a little different. 
+// Any JSX element can be written with a self-closing tag, 
+// and every element must be closed. The line-break tag, for example, 
+// must always be written as <br />
+
+const comp = <div>
+    <h2>Welcome to React</h2><br/>
+    <p>Be sure to close all tags!</p>
+    <hr/>
+</div>
