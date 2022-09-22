6c8c0d2cf4d17f0a185e19443dd029b54822cfd4
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-22 19:29:28 +0300

    I have reset the code for the App.js file

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,48 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'Initial State'
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    // Change code below this line
-    this.setState("Enock")
-    // Change code above this line
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>Click Me</button>
-        <h1>{this.state.name}</h1>
-      </div>
-    );
-  }
-};
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: 'Initial State'
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+//   handleClick() {
+//     // Change code below this line
+//     this.setState({name: "Enock Male"})
+//     // Change code above this line
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.handleClick}>Click Me</button>
+//         <h1>{this.state.name}</h1>
+//       </div>
+//     );
+//   }
+// };
 
 
 // This means that this is a stateless component.
-// function App() {
-//   return (
-//     // <div className="App" id='main-element'>
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <h1 className='title-h1'>This is my first program</h1>
-//     //     <a
-//     //       className="App-link"
-//     //       href="https://reactjs.org"
-//     //       target="_blank"
-//     //       rel="noopener noreferrer"
-//     //     >
-//     //       Learn React
-//     //     </a>
-//     //   </header>
-//     // </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App" id='main-element'>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className='title-h1'>This is my first program</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    // <div className='App'>
+    //   <p>I love programming in Javascript</p>
+    // </div>
+  );
+}
 
 export default App;
