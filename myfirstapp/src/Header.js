01006c3465a5d4c75144fd696d8ca991323033bd
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-24 00:18:16 +0300

    import the css file in the final project

--- myfirstapp/src/Header.js
+++ myfirstapp/src/Header.js
@@ -1,12 +1,10 @@
-import '/sytle.css'
-
 function Second(props){
     return (
         <div>
             <h2>This is my second component</h2>
             <p>{props.name}</p>
         </div>
-    )
+    );
 }
 
 // Setting a default value for the props
