adfe675e804c148055bd1dbf6718dc0befcdd7c2
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-29 13:09:57 +0300

    I have commented all the code in the App.js file

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,57 +1,57 @@
-// I have imported react to this file.
-import React from 'react'
-// import '/sytle.css'
-import Second from './Header';
-import Footer from './Footer';
+// // I have imported react to this file.
+// import React from 'react'
+// // import '/sytle.css'
+// import Second from './Header';
+// import Footer from './Footer';
 
-import './App.css';
+// import './App.css';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    // This is how you create a state in React.
-    this.state = {
-      name: 'We have to work on something now',
-      age:0,
-      occupation:"Your Job",
-      visibility: false
-    };
-    this.handleClick = this.handleClick.bind(this); // This binds the this to the method
-    // this.myMethod = this.myMethod.bind(this)
+//     // This is how you create a state in React.
+//     this.state = {
+//       name: 'We have to work on something now',
+//       age:0,
+//       occupation:"Your Job",
+//       visibility: false
+//     };
+//     this.handleClick = this.handleClick.bind(this); // This binds the this to the method
+//     // this.myMethod = this.myMethod.bind(this)
 
-    this.toggleVisibility = this.toggleVisibility.bind(this)// Binding this to the toggleVisibility class.
+//     this.toggleVisibility = this.toggleVisibility.bind(this)// Binding this to the toggleVisibility class.
 
-  }
+//   }
 
-  // This is a click function 
-  handleClick() {
-    // Change code below this line
-    this.setState({name: "Enock"})
-    // Change code above this line
-  }
+//   // This is a click function 
+//   handleClick() {
+//     // Change code below this line
+//     this.setState({name: "Enock"})
+//     // Change code above this line
+//   }
 
-  // This is also a click function 
-  toggleVisibility(){
-    this.setState(state=>{
-      if(state.visibility === true){
-        return {visibility:false}
-      }else{
-        return {visibility:true}
-      }
-    })
-  }
-  render() {
-    return (
-      <div>
-        <Second name="Enock Developer"/>
-        <button onClick={this.handleClick}>Click Me</button>
-        <h1>{this.state.name}</h1>
-        {/* <Footer/> */}
-      </div>
-    );
-  }
-};
+//   // This is also a click function 
+//   toggleVisibility(){
+//     this.setState(state=>{
+//       if(state.visibility === true){
+//         return {visibility:false}
+//       }else{
+//         return {visibility:true}
+//       }
+//     })
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <Second name="Enock Developer"/>
+//         <button onClick={this.handleClick}>Click Me</button>
+//         <h1>{this.state.name}</h1>
+//         {/* <Footer/> */}
+//       </div>
+//     );
+//   }
+// };
 
 
 // This means that this is a stateless component.
