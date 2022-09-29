302701becc5d5a27a9d6856b67811afdf1c8125a
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-29 13:18:10 +0300

    I have created the display that I want on my page

--- myfirstapp/src/Footer.js
+++ myfirstapp/src/Footer.js
@@ -4,4 +4,20 @@ export default class Footer extends React.Component{
     constructor(props){
         super(props)
     }
+
+    render(){
+        return(
+            <div className="container">
+                <div>
+                    <h1>Welcome to Enock</h1>
+                    <div className="left-side">
+
+                    </div>
+                    <div className="right-side">
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
