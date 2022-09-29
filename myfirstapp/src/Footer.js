16304640a2a230347d94d36e648ef96ca03c1057
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-29 13:23:56 +0300

    Designed the frontend very well

--- myfirstapp/src/Footer.js
+++ myfirstapp/src/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Footer.css'
 
 export default class Footer extends React.Component{
     constructor(props){
@@ -10,11 +11,13 @@ export default class Footer extends React.Component{
             <div className="container">
                 <div>
                     <h1>Welcome to Enock</h1>
-                    <div className="left-side">
-
-                    </div>
-                    <div className="right-side">
-
+                    <div className="inner-container">
+                        <div className="left-side">
+                            <p>Left-side</p>
+                        </div>
+                        <div className="right-side">
+                            <p>Right-side</p>
+                        </div>
                     </div>
                 </div>
             </div>
