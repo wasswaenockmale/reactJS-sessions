d8e5231f43d1c1a261e3033a60e5a2f4ef467193
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-29 13:07:05 +0300

    How to update the state of the component by passing a function to the setState function

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 // I have imported react to this file.
 import React from 'react'
 // import '/sytle.css'
 import Second from './Header';
+import Footer from './Footer';
 
 import './App.css';
 
@@ -13,27 +13,41 @@ export default class App extends React.Component {
     // This is how you create a state in React.
     this.state = {
       name: 'We have to work on something now',
-      age:23,
-      Occupation: "Write your occupation here"
+      age:0,
+      occupation:"Your Job",
+      visibility: false
     };
     this.handleClick = this.handleClick.bind(this); // This binds the this to the method
     // this.myMethod = this.myMethod.bind(this)
+
+    this.toggleVisibility = this.toggleVisibility.bind(this)// Binding this to the toggleVisibility class.
+
   }
+
+  // This is a click function 
   handleClick() {
     // Change code below this line
-    this.setState({name: "Enock", 
-    age:24,
-    Occupation: "Programmer."
-  })
+    this.setState({name: "Enock"})
     // Change code above this line
+  }
+
+  // This is also a click function 
+  toggleVisibility(){
+    this.setState(state=>{
+      if(state.visibility === true){
+        return {visibility:false}
+      }else{
+        return {visibility:true}
+      }
+    })
   }
   render() {
     return (
       <div>
-        <Second name="Nalongo Sarah"/>
+        <Second name="Enock Developer"/>
         <button onClick={this.handleClick}>Click Me</button>
         <h1>{this.state.name}</h1>
-        <Second/>
+        {/* <Footer/> */}
       </div>
     );
   }
