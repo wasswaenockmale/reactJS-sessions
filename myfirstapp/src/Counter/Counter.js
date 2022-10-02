5e2ab6c8226ec4742221d72e32f948f2befa4b25
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-02 12:15:01 +0300

    I have created a functional stateless component called Counter

---
+++ myfirstapp/src/Counter/Counter.js
@@ -0,0 +1,10 @@
+function Counter(props){
+    // now, I am working on the counter section.
+    return(
+        <div>
+            <p>Counter<span>0</span> </p>
+        </div>
+    )
+}
+
+export default Counter
