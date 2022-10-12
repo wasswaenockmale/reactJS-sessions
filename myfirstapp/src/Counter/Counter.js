03b204f1b60f4f1d6aeb53ce91d59a68a3cc8a0c
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-12 17:37:46 +0300

    I have returned the right state for the increment()

--- myfirstapp/src/Counter/Counter.js
+++ myfirstapp/src/Counter/Counter.js
@@ -2,7 +2,7 @@ function Counter(props){
     // now, I am working on the counter section.
     return(
         <div>
-            <p>Counter: <span>{props.counter}</span> </p>
+            <p>Counter: <span>{props.count}</span> </p>
         </div>
     )
 }
