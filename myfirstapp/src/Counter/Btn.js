7abe9caad6a17a54ec50dd70aa1674f5c713d8ed
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-21 19:39:03 +0300

    Added a form that I have handled

--- myfirstapp/src/Counter/Btn.js
+++ myfirstapp/src/Counter/Btn.js
@@ -9,13 +9,15 @@ export default class Btn extends React.Component{
 
         this.state = {
             counter: 0,
-            input:"Zero"
+            input:"",
+            submit:""
         }
 
         this.increment = this.increment.bind(this);
         this.decrement = this.decrement.bind(this);
         this.reset = this.reset.bind(this);
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     increment(){
@@ -49,16 +51,35 @@ export default class Btn extends React.Component{
             input:event.target.value
         })
     }
+
+
+    handleSubmit(event){
+        event.preventDefault();
+        return this.setState({
+            submit:this.state.input
+        })
+    }
     render(){
         return(
             <div className="container">
-                <input placeholder="Enter something here" value={this.state.input} onChange={this.handleChange}></input>
-                <button onClick={this.increment}>Increment</button>
-                <button onClick={this.decrement}>Decrement</button>
-                <button onClick={this.reset}>Reset</button>
                 <div>
-                    <Counter count={this.state.counter}/>
-                    <p>{this.state.input}</p>
+                    {/* <input placeholder="Enter something here" value={this.state.input} onChange={this.handleChange}></input> */}
+                    <button onClick={this.increment}>Increment</button>
+                    <button onClick={this.decrement}>Decrement</button>
+                    <button onClick={this.reset}>Reset</button>
+                    <div>
+                        <Counter count={this.state.counter}/>
+                        {/* <p>{this.state.input}</p> */}
+                    </div>
+                </div>
+
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input value={this.state.input} onChange={this.handleChange}></input>
+                        <button type="submit">submit!</button>
+                    </form>
+
+                    <h1>{this.state.submit}</h1>
                 </div>
             </div>
         )
