10d41e079ac452e409d7ad098145a88fae39779f
wasswaenockmale <enockmale97@gmail.com>
2023-02-15 08:21:47 +0300

    I have changed the value

--- src/index.js
+++ src/index.js
@@ -4,5 +4,3 @@ import App from './Components/App/'
 
 // const header1 = <h1>Hello programmer, The time is: {getCurrentDate()}</h1>;
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
-// const greeting = React.createElement('h1',{}, "Hello programmer!");
-// ReactDOM.createRoot(document.getElementById('root')).render(header1);
